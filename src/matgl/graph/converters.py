fb1911afd7ff3f0e8219d4e5aa01e6bf5f95ddff
Anthony Onwuli <anthony.onwuli16@imperial.ac.uk>
2024-08-20 21:21:53 +0100

    Add species types to converters

--- src/matgl/graph/converters.py
+++ src/matgl/graph/converters.py
@@ -32,6 +32,7 @@ class GraphConverter(metaclass=abc.ABCMeta):
         lattice_matrix,
         element_types,
         frac_coords,
+        species_types=None,
         is_atoms: bool = False,
     ) -> tuple[dgl.DGLGraph, torch.Tensor, list]:
         """Construct a dgl graph from processed structure and bond information.
@@ -44,12 +45,17 @@ class GraphConverter(metaclass=abc.ABCMeta):
             lattice_matrix: lattice information of the structure.
             element_types: Element symbols of all atoms in the structure.
             frac_coords: Fractional coordinates of all atoms in the structure. Note: Cartesian coordinates for molecule
+            species_types: Species symbols of all ions in the structure.
+                Note: Only one of element_types or species_types can be provided.
             is_atoms: whether the input structure object is ASE atoms object or not.
 
         Returns:
             DGLGraph object, state_attr
 
         """
+        # Check that only one of element_types or species_types is provided
+        assert (element_types is not None) ^ (species_types is not None)
+
         u, v = torch.tensor(src_id), torch.tensor(dst_id)
         g = dgl.graph((u, v), num_nodes=len(structure))
         # TODO: Need to check if the variable needs to be double or float, now use float
@@ -58,12 +64,20 @@ class GraphConverter(metaclass=abc.ABCMeta):
         # TODO: Need to check if the variable needs to be double or float, now use float
         lattice = torch.tensor(np.array(lattice_matrix), dtype=matgl.float_th)
         # Note: pbc_ offshift and pos needs to be float64 to handle cases where bonds are exactly at cutoff
-        element_to_index = {elem: idx for idx, elem in enumerate(element_types)}
-        node_type = (
-            np.array([element_types.index(site.specie.symbol) for site in structure])
-            if is_atoms is False
-            else np.array([element_to_index[elem] for elem in structure.get_chemical_symbols()])
-        )
+        if element_types:
+            element_to_index = {elem: idx for idx, elem in enumerate(element_types)}
+            node_type = (
+                np.array([element_types.index(site.specie.symbol) for site in structure])
+                if is_atoms is False
+                else np.array([element_to_index[elem] for elem in structure.get_chemical_symbols()])
+            )
+        else:
+            species_to_index = {specie: idx for idx, specie in enumerate(species_types)}
+            node_type = (
+                np.array([species_types.index(site.specie.to_pretty_string()) for site in structure])
+                if is_atoms is False
+                else np.array([species_to_index[specie] for specie in structure.get_atomic_numbers()])
+            )
         g.ndata["node_type"] = torch.tensor(node_type, dtype=matgl.int_th)
         # TODO: Need to check if the variable needs to be double or float, now use float
         g.ndata["frac_coords"] = torch.tensor(frac_coords, dtype=matgl.float_th)
